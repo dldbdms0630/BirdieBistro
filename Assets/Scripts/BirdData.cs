4ddf71a29da04e344e24ef16802e15ac7d6fcd86
dldbdms0630 <dldbdms0630@gmail.com>
2024-10-26 15:22:42 -0400

    birds and dialogue
    birds inputted and dialogue box and textbox set for UI

---
+++ Assets/Scripts/BirdData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BirdData : MonoBehaviour
+{
+    public string birdName;
+    public string dialogueText; // What the bird says
+    public string preferredCuisine; // Example: "Italian", "Spicy", etc.
+}
