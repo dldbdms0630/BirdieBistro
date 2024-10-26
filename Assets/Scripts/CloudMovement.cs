1686e5e7d4b2ab73f3414e6ecfb62b2014ec4cf2
dldbdms0630 <dldbdms0630@gmail.com>
2024-10-26 13:48:22 -0400

    moving cloud script
    clouds move horizontally; can reference for other transform functions (might revisit but later when other bird stuff is finished)

--- Assets/Scripts/CloudMovement.cs
+++ Assets/Scripts/CloudMovement.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class CloudMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // public float minSpeed = 0.01f;
+    // public float maxSpeed = 0.05f;
+    public int minSpeed = 0;
+    public int maxSpeed = 20;
+    public float endPositionX = 20f; // x pos to where the cloud will loop back
+    private Vector3 startPosition;
+
     void Start()
     {
-        
+        // set initial pos (based on pos of cloud sprite)
+        startPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float randomSpeed = Random.Range(minSpeed, maxSpeed);
+
+        // move cloud horizontally by small "speed" each frame
+        transform.position += new Vector3(randomSpeed, 0, 0) * Time.deltaTime;
+
+        // reset back to starting point if cloud goes beyond end pos 
+        if (transform.position.x > endPositionX)
+        {
+            transform.position = new Vector3(startPosition.x, transform.position.y, transform.position.z);
+        }
     }
 }
