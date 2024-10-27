1c7b36d5f7bfbf166b3a7c453b1fa7d853fd2735
dldbdms0630 <dldbdms0630@gmail.com>
2024-10-26 21:35:42 -0400

    invite button updated
    initially has no birds and so we can invite a single new bird. able to click on feed button and something will change.

--- Assets/Scripts/BirdHandler.cs
+++ Assets/Scripts/BirdHandler.cs
@@ -6,17 +6,19 @@ using TMPro;
 
 public class BirdHandler : MonoBehaviour
 {
-    public BirdData birdData; 
+    public BirdData.Bird birdData; 
     public GameObject speechBubble;
     public TextMeshProUGUI dialogueText; // make into textmeshpro instead?? 
     public Button recipeInputButton;
     public GameObject mealForm;
+
     public Vector3 zoomedInScale = new Vector3(1.1f, 1.1f, 1f);
     public Vector3 zoomedInPosition = new Vector3(-4.14f, -1.3f, -1.44f);
     public Sprite happySprite;
 
     private Vector3 originalPosition;
     private Vector3 originalScale;
+    // private BirdData.Bird specificBird;
 
     private bool isZoomedIn = false;
     private string birdName; 
@@ -28,11 +30,26 @@ public class BirdHandler : MonoBehaviour
         originalScale = transform.localScale; // store originalScale for resetting 
         originalPosition = transform.position;
         // hide UI initially 
-        speechBubble.SetActive(false);
-        dialogueText.gameObject.SetActive(false);
-        recipeInputButton.gameObject.SetActive(false);
-        mealForm.SetActive(false);
+        // speechBubble.SetActive(false);
+        // dialogueText.gameObject.SetActive(false);
+        // recipeInputButton.gameObject.SetActive(false);
+        // mealForm.SetActive(false);
         birdName = gameObject.name;
+        // specificBird = birdData.BirdData.Bird;
+    }
+
+    public void AssignBirdData(BirdData.Bird data, GameObject speechBubble, TextMeshProUGUI dialogueText, Button recipeInputButton, GameObject mealForm)
+    {
+        Debug.Log("Data Assigned.");
+        // specificBird = data;
+        birdData = data;
+        this.speechBubble = speechBubble;
+        this.dialogueText = dialogueText;
+        this.recipeInputButton = recipeInputButton;
+        this.mealForm = mealForm;
+
+        Debug.Log($"SpeechBubble: {speechBubble}, DialogueText: {dialogueText}, RecipeInputButton: {recipeInputButton}, MealForm: {mealForm}");
+
     }
 
     void OnMouseDown()
@@ -55,14 +72,17 @@ public class BirdHandler : MonoBehaviour
         transform.localScale = zoomedInScale;
         transform.position = zoomedInPosition;
         isZoomedIn = true;
+        
         speechBubble.SetActive(true);
         dialogueText.gameObject.SetActive(true);        
         recipeInputButton.gameObject.SetActive(true);
 
+        dialogueText.text = birdData.dialogue;
+
         recipeInputButton.onClick.AddListener(OnMealFormClicked);
         
 
-        dialogueText.text = birdData.GetBirdByName(birdName).dialogue;
+        // dialogueText.text = birdData.GetBirdByName(birdName).dialogue;
     }
 
     void OnMealFormClicked()
--- Assets/Scripts/BirdSpawner.cs
+++ Assets/Scripts/BirdSpawner.cs
@@ -1,22 +1,98 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class BirdSpawner : MonoBehaviour
 {
 
-    public BirdDatabase birdDatabase; 
-    public GameObject birdPrefab;
-    public Transform 
-    // Start is called before the first frame update
+    public BirdData birdDatabase; 
+    // public GameObject birdPrefab;
+    // public Transform spawnArea;
+    public Button inviteButton;
+
+    [SerializeField] private GameObject speechBubble;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private Button recipeInputButton;
+    [SerializeField] private GameObject mealForm;
+    
     void Start()
     {
-        
+        inviteButton.onClick.AddListener(InviteNewBird);
+        UpdateInviteButton();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void InviteNewBird()
     {
+        // Debug.Log("BirdInvited");
+        if (NoBirdsInScene())
+        {
+            BirdData.Bird randomBirdData = GetRandomBirdData();
+            SpawnBird(randomBirdData); // invite the random bird 
+        }
+        UpdateInviteButton(); // disable button after bird is invited 
+    }
+
+    private BirdData.Bird GetRandomBirdData()
+    {
+        int randomIndex = Random.Range(0, birdDatabase.birds.Count);
+        return birdDatabase.birds[randomIndex];
+    }
+
+    private void SpawnBird(BirdData.Bird birdData)
+    {
+        if (birdData.birdPrefab != null)
+        {
+
+            GameObject bird = Instantiate(birdData.birdPrefab, GetRandomPosition(),
+                Quaternion.identity);
+
+            BirdHandler birdHandler = bird.GetComponent<BirdHandler>();
+            birdHandler.AssignBirdData(birdData, speechBubble, dialogueText, recipeInputButton, mealForm);
+
+
+            // GameObject speechBubble = GameObject.Find("DialogueBox"); // Adjust the name to match the actual object in your scene.
+            
+            
+            // if (speechBubble == null)
+            // {
+            //     Debug.LogWarning("Speech Bubble not found!");
+            // }
+            // else{
+            //     Debug.Log(" speech bubble found!!");
+            // }
         
+            // TextMeshProUGUI dialogueText = GameObject.Find("DialogueText")?.GetComponent<TextMeshProUGUI>();
+            // Button recipeInputButton = GameObject.Find("Feed")?.GetComponent<Button>();
+            // GameObject mealForm = GameObject.Find("MealForm");
+
+        } else 
+            Debug.LogWarning($"BirdData for {birdData.name} does not have an assigned prefab.");
+    }
+
+    private Vector3 GetRandomPosition()
+    {
+        // Define limits based on screen size or a predefined area.
+        float xMin = -5f; // Example values, adjust based on your scene.
+        float xMax = 5f;
+        float yMin = -3f;
+        float yMax = 1f;
+
+        float randomX = Random.Range(xMin, xMax);
+        float randomY = Random.Range(yMin, yMax);
+
+        return new Vector3(randomX, randomY, -1f);
+    }
+
+    private bool NoBirdsInScene()
+    {
+        return GameObject.FindObjectsOfType<BirdHandler>().Length == 0;
+    }
+
+    private void UpdateInviteButton()
+    {
+        inviteButton.gameObject.SetActive(NoBirdsInScene());
     }
 }
